2283ee030c613ffb505fad6a1e88214da0a8124a
ishag2 <64750301+ishag2@users.noreply.github.com>
2025-12-31 10:05:59 -0600

    Fixed test 4

--- tests/test_axitoapb_hidden.py
+++ tests/test_axitoapb_hidden.py
@@ -169,7 +169,7 @@ async def test_4(dut):
     await Timer(10, unit="ns")  
     dut.wvalid.value = 1
     dut.wlast.value = 1
-    dut.wdata.value = 20
+    dut.wdata.value = 9
     await Timer(20, unit="ns")  
     dut.bready.value = 1
     await Timer(40, unit="ns")  
@@ -179,7 +179,7 @@ async def test_4(dut):
     dut._log.info("APB address = %d, pwdata = %d",
                   dut.paddr.value, dut.pwdata.value)
     assert dut.paddr.value == 2, "APB address is not correct"   
-    assert dut.pwdata.value == 20, "APB write data is not correct" 
+    assert dut.pwdata.value == 9, "APB write data is not correct" 
     await Timer(40, unit="ns")  
     dut.pready.value = 0
     
